84b97532fc67ee467b1382a14b1cf6afb33a53f8
creed2win <michal.dovole@gmail.com>
2024-10-13 15:43:49 +0200

    calling API but still has to unmarshal into a proper structure

--- commandMap.go
+++ commandMap.go
@@ -1,7 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func commandMap() error {
+	request, err := http.NewRequest("GET", "https://pokeapi.co/api/v2/location/?offset=20&limit=20", nil)
+	client := http.Client{}
+	if err != nil {
+		return fmt.Errorf("error making request: ", err)
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Errorf("error getting respose: ", err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+
+	//var data string
+
+	//json.Unmarshal(bodyBytes, &data)
+
+	fmt.Println("printing data: ", string(bodyBytes))
+
 	return nil
 }
 
-//TODO - implement GET from https://pokeapi.co/api/v2/location/ with pagination of 20 items (that is API default behaviour)
+//TODO - need to create a data structure to store locations
+//then I can unmarshal that data into it.
